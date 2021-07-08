df81fcad0d15d567eb2b6b43946bc7e48f05ebbc
Ricardo <ricardogrijalva10@gmail.com>
2021-07-07 20:53:25 -0600

    Mostrar Productos a Administrador

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/main.css')}} ">
+    @yield('css')
 </head>
 <body>
     <div id="app">
@@ -77,7 +79,12 @@
 
         <main class="py-4">
             @yield('content')
+            @yield('adminProductos')
         </main>
     </div>
+    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    @yield('js')
 </body>
 </html>
